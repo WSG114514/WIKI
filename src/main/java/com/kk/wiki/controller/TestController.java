eac4d5f0242994d9f3fcfcd06211d43f1544f418
WSG114514 <k2575693253@gmail.com>
2021-12-13 18:54:06 +0800

    测试application自定义项

--- src/main/java/com/kk/wiki/controller/TestController.java
+++ src/main/java/com/kk/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.kk.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String hol;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return hol + " World";
     }
 }
