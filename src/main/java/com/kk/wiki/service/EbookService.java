887a15b4cd8c4ebc81b6b2425cfdddc02bfe7a46
WSG114514 <k2575693253@gmail.com>
2021-12-18 11:36:13 +0800

    使用Vue CLI生成web项目

--- src/main/java/com/kk/wiki/service/EbookService.java
+++ src/main/java/com/kk/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.kk.wiki.domain.EbookExample;
 import com.kk.wiki.mapper.EbookMapper;
 import com.kk.wiki.req.EbookReq;
 import com.kk.wiki.resp.EbookResp;
+import com.kk.wiki.utils.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +27,7 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
 
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
 
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
