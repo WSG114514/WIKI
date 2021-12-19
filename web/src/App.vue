865d57870f81a1748c60abb648ea246d7f60b039
WSG114514 <k2575693253@gmail.com>
2021-12-19 21:11:46 +0800

    制作the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,43 +1,26 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-    <router-view></router-view>
+    <the-header></the-header>
+    <router-view/>
     <a-layout-footer style="text-align: center">
       Ant Design ©2018 Created by Ant UED
     </a-layout-footer>
   </a-layout>
 </template>
+
 <script lang="ts">
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+
+import TheHeader from '@/components/the-header.vue';
+
 export default defineComponent({
+  name : 'app',
   components: {
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  },
-  setup() {
-    return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
-    };
+    TheHeader,
   },
 });
 </script>
+
 <style>
 #components-layout-demo-top-side-2 .logo {
   float: left;
