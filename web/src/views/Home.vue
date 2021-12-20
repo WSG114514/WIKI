05f91bea7fed770e53e71b5b891efcca8462c80e
WSG114514 <k2575693253@gmail.com>
2021-12-20 13:13:31 +0800

    集成axios

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -53,12 +53,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
   },
 });
 </script>
