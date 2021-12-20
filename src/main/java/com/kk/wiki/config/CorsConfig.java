3e3cc955764a17f39e956c6043eeaf165dd12ec0
WSG114514 <k2575693253@gmail.com>
2021-12-20 13:50:58 +0800

    设置CorsConfig配置类，解决前后端分离项目的请求跨域问题

---
+++ src/main/java/com/kk/wiki/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.kk.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);//1小时内不需要再次预检(发送OPTIONS请求)
+    }
+}
