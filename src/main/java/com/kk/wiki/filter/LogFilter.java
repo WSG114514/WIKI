cacb90611f6bfd704860f9c109b61e6a9be1a3ac
WSG114514 <k2575693253@gmail.com>
2021-12-21 13:34:04 +0800

    filter过滤器

---
+++ src/main/java/com/kk/wiki/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package com.kk.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.Locale;
+
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    /**
+     * 可以初始化Filter在web.xml里面配置的初始化参数
+     * filter对象只会创建一次，init方法也只会执行一次。
+     * @param filterConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("---------------------------LOG开始-----------------------------");
+        LOG.info("请求地址：{} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("远程地址：{}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        long endTime = System.currentTimeMillis();
+        LOG.info("---------------------------LOG结束--耗时：{}ms------------------", endTime - startTime);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
