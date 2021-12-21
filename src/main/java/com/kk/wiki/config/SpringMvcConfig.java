727092eadfda582d1f52d38dd16ade35b2145b5f
WSG114514 <k2575693253@gmail.com>
2021-12-21 15:23:44 +0800

    怎加AOP，注释过滤器，拦截器

--- src/main/java/com/kk/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/kk/wiki/config/SpringMvcConfig.java
@@ -1,19 +1,19 @@
-package com.kk.wiki.config;
-
-import com.kk.wiki.interceptor.LogIntercepter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogIntercepter logIntercepter;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logIntercepter).addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.kk.wiki.config;
+//
+//import com.kk.wiki.interceptor.LogIntercepter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogIntercepter logIntercepter;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logIntercepter).addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
