486fc5e0ffcd5e81d25f6880d8cf9c349cf21332
WSG114514 <k2575693253@gmail.com>
2021-12-24 21:34:22 +0800

    PageHelper设置，管理頁面編寫

--- src/main/java/com/kk/wiki/service/EbookService.java
+++ src/main/java/com/kk/wiki/service/EbookService.java
@@ -1,5 +1,6 @@
 package com.kk.wiki.service;
 
+import com.github.pagehelper.PageHelper;
 import com.kk.wiki.domain.Ebook;
 import com.kk.wiki.domain.EbookExample;
 import com.kk.wiki.mapper.EbookMapper;
@@ -23,6 +24,8 @@ public class EbookService {
 
     public List<EbookResp> list(EbookReq req) {
 
+        //PageHelper在查詢一次后就會失效**** （）
+        PageHelper.startPage(2, 3);
         EbookExample ebookExample = new EbookExample();
         //createCriteria 相当于创建一个where条件
         EbookExample.Criteria criteria = ebookExample.createCriteria();
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,168 @@
 <template>
-  <div class="about">
-    <h1>电子书管理</h1>
-  </div>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+              <span>
+                <user-outlined />
+                subnav 1
+              </span>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+              <span>
+                <notification-outlined />
+                subnav 3
+              </span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <a-table
+          :columns="columns"
+          :data-source="ebook"
+          :row-key="record => record.id"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover}">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+
+        <template v-slot:action="{ text, record}">
+          <a-space size="small">
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
 </template>
+<script lang="ts">
+import { defineComponent, onMounted,ref } from 'vue';
+import axios from "axios";
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 2,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: { customRender: 'cover'}
+      },
+      {
+        title: '分类1',
+        key: 'category1Id',
+        dataIndex: 'category1Id',
+      },
+      {
+        title: '分类2',
+        key: 'category2Id',
+        dataIndex: 'category2Id',
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount',
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount',
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      },
+    ];
+
+    /**
+     * 数据查询
+     */
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      axios.get("/ebook/list", params).then((response) => {
+        loading.value = false;
+        ebooks.value = response.data;
+
+        //重置分页配置
+        pagination.value.current = params.page;
+      });
+    }
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("pagination" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    }
+
+    onMounted(()=> {
+      handleQuery({});
+    })
+
+    return {
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    };
+  },
+  components: {
+
+  },
+});
+
+
+</script>
