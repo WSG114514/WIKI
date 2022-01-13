e8007db888f01041c73c374d3d20cf859bac2380
WSG114514 <k2575693253@gmail.com>
2022-01-13 17:52:07 +0800

    点击编辑按钮跳出提示框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -15,7 +15,7 @@
 
         <template v-slot:action="{ text, record}">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit">
               编辑
             </a-button>
             <a-button type="danger">
@@ -26,6 +26,15 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOK"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 <script lang="ts">
 import { defineComponent, onMounted,ref } from 'vue';
@@ -113,6 +122,24 @@ export default defineComponent({
       });
     }
 
+    //---------------------表单-----------------------
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOK = ()=> {
+      modalLoading.value = true;
+      setTimeout(()=>{
+        modalVisible.value = false;
+        modalLoading.value = true;
+      }, 2000);
+    }
+
+    /**
+     * 编辑
+     */
+    const edit = () => {
+      modalVisible.value = true;
+    }
+
     onMounted(()=> {
       handleQuery({
         page: 1,
@@ -125,7 +152,12 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+
+      edit,
+      modalVisible,
+      modalLoading,
+      handleModalOK
     };
   },
   components: {
