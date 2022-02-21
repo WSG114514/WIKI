08b4ab15ddcb1de1ec86f05997f56f2ecf3eaf7c
WSG114514 <k2575693253@gmail.com>
2022-02-21 12:54:30 +0800

    增加路由登录拦截

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -5,6 +5,8 @@ import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -29,22 +31,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategory',
-    component: AdminCategory
+    component: AdminCategory,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
-    component: AdminDoc
+    component: AdminDoc,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta: {
+      loginRequire: true
+    }
   }
 ]
 
@@ -52,5 +66,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做拦截监控
+  if(to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验", item.meta.loginRequire);
+    return item.meta.loginRequire;
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isNotEmpty(loginUser)) {
+      console.log("用户未登录!");
+      next('/');
+    }else {
+      next();
+    }
+  }else {
+    next();
+  }
+});
 
 export default router
